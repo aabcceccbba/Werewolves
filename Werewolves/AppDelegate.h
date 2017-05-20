1cf1fdf2d4a5f34d3112dc0c0565ea002dbd7fc4
aabcceccbba <kaiyuelu@gmail.com>
2017-05-20 03:34:22 -0400

    Initial Commit

---
+++ Werewolves/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Werewolves
+//
+//  Created by Kay Lu on 5/20/17.
+//  Copyright © 2017 a. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Werewolves/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Werewolves
+//
+//  Created by Kay Lu on 5/20/17.
+//  Copyright © 2017 a. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
